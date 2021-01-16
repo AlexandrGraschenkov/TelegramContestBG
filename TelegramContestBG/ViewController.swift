30764aa24795d17a9ad710de1b3a53bff59ad61e
Alexander Graschenkov <sharrikk@gmail.com>
2021-01-16 10:26:39 +0300

    - init

---
+++ TelegramContestBG/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TelegramContestBG
+//
+//  Created by Alexander Graschenkov on 16.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
